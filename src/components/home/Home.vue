4d20b696358cc2f8675219929e0faffbf96779c7
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2018-10-15 14:18:18 +0900

    feat:Topページ作成

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,5 +1,8 @@
 <template src="./home.pug" lang="pug">
 </template>
 
+<style src="./home.scss" lang="scss">
+</style>
+
 <script src="./home_script.ts" lang="ts">
 </script>
--- src/components/home/home_script.ts
+++ src/components/home/home_script.ts
@@ -2,5 +2,5 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component({})
 export default class Home extends Vue {
-  title: string = "Welcome isryo Introduction Page!!!";
+  title: string = "Welcome isryo Introduction Site.";
 }
