7e537de3ec0511f27c52b59353f5bbdb6d69cf64
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2018-10-16 14:02:34 +0900

    feat:Contactページを作成

--- src/components/contact/contact_script.ts
+++ src/components/contact/contact_script.ts
@@ -2,5 +2,10 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component({})
 export default class Contact extends Vue {
-  title: string = "Contact Page.";
+  title: string = "ご連絡は下記GmailかTwitterDMからよろしくお願いいたします";
+  gmailAddress: string = "ryosuke.izumi62@gmail.com";
+  twitterInfo = {
+    name: "is_ryo",
+    link: "https://twitter.com/is_ryo"
+  }
 }
